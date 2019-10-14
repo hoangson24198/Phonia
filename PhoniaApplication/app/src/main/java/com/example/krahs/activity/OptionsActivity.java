71d003ec4c8e8546020f3c02a6041be1355d44a9
le.son <hoangson24198@gmail.com>
2019-10-15 00:20:38 +0700

    first commit

---
+++ PhoniaApplication/app/src/main/java/com/example/krahs/activity/OptionsActivity.java
@@ -0,0 +1,48 @@
+package com.example.krahs.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.krahs.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class OptionsActivity extends AppCompatActivity {
+
+    TextView logout;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Options");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        mAuth = FirebaseAuth.getInstance();
+
+        logout = findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                Intent loginIntent = new Intent(OptionsActivity.this, LoginActivity.class);
+                loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(loginIntent);
+                finish();
+            }
+        });
+    }
+}
